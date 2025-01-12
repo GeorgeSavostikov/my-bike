55727ecf1b8faf2de76062b07ee198761ee40492
Heorhii Savostikov <georgesavostikov@gmail.com>
2025-01-12 02:27:51 +0200

    make build

---
+++ index.e1a8b411.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector(".myForm").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.e1a8b411.js.map
